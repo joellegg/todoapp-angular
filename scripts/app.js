8469f104e3d8e91a0f8a58b03c104ccbee5c1cc3
joellegg <joellegg@bellsouth.net>
2017-01-14 09:17:03 -0600

    added services to get data

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,27 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+
   $scope.learningNgChange = function() {
     console.log("An input changed!");
   };
-  
-  $scope.todos = [
-    {"name": "clean the house"},
-    {"name": "water the dog"},
-    {"name": "feed the lawn"},
-    {"name": "pay the cook"},
-    {"name": "fart"},
-    {"name": "clean car"}
-  ]
+
+  $scope.hellowWorld = dataService.helloWorld;
+
+  dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data;
+  })
+})
+// services are used to request data from a server and/or store user information
+// http provider
+.service('dataService', function($http) {
+  this.helloConsole = function() {
+    console.log('This is the hello console service!');
+  };
+
+  this.getTodos = function(callback) {
+    $http.get('mock/todos.json')
+    .then(callback)
+  }
 });
