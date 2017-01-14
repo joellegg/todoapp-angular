0090dd797b272b0f9bbe7001347530623b12487b
joellegg <joellegg@bellsouth.net>
2017-01-14 09:47:45 -0600

    added services to save and delete todos

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 angular.module("todoListApp", [])
+
 .controller('mainCtrl', function($scope, dataService) {
   $scope.helloConsole = dataService.helloConsole;
 
@@ -6,22 +7,34 @@ angular.module("todoListApp", [])
     console.log("An input changed!");
   };
 
-  $scope.hellowWorld = dataService.helloWorld;
-
   dataService.getTodos(function(response) {
     console.log(response.data);
     $scope.todos = response.data;
-  })
+  });
+
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    // this deletes the todo
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo)
+  }
 })
 // services are used to request data from a server and/or store user information
 // http provider
 .service('dataService', function($http) {
-  this.helloConsole = function() {
-    console.log('This is the hello console service!');
-  };
-
   this.getTodos = function(callback) {
     $http.get('mock/todos.json')
     .then(callback)
-  }
+  };
+
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been deleted!")
+  };
+
+  this.saveTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been saved!")
+  };
 });
